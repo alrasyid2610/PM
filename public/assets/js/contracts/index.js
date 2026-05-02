bb0b7961f4e2fd94997382fd4b8f4d2355f3bb86
Muhammad Daniyal Rosil <tkarlsefni7@gmail.com>
2026-05-02 14:39:36 +0700

    maintenance contract

--- public/assets/js/contracts/index.js
+++ public/assets/js/contracts/index.js
@@ -4,12 +4,6 @@ $(document).ready(function () {
     page = new CrudPageController({
         primaryKey: "id_contract",
         renderForm: renderForm,
-        initSelect: function () {
-            $("#detail_kelompok").select2({
-                width: "100%",
-                dropdownParent: $("#detailContent"),
-            });
-        },
         useAttachment: true,
     });
 });
